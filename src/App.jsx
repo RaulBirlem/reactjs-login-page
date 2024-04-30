76ef5916bb377ff57458b938cfef22be8a6a7a44
RaulBirlem <raul.birlem@gmail.com>
2024-04-30 17:25:42 -0300

    input forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Login from './components/login/Login'
 
 function App() {
 
 
   return (
     <div className="App">
-      <p>Página de Login</p>
+      <Login/>
     </div>
   )
 }
